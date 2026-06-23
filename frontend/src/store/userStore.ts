ec6e9e016f60b9039d4b0ce1918b8bb4d70b49e9
Nuona Liang <lnuo026@aucklanduni.ac.nz>
2026-06-23 12:45:59 +1200

    feat: add zustand

---
+++ frontend/src/store/userStore.ts
@@ -0,0 +1,23 @@
+import { create } from 'zustand'
+
+interface User {
+     id: string
+     email: string
+     name: string
+     picture: string
+}
+
+// 定义"这个仓库长什么样"
+interface UserStore {
+     user: User | null
+     setUser: (user: User) => void
+     clearUser: () => void
+}
+
+// 真正创建仓库
+// (set) => ({...}) — set 是 Zustand 给你的"修改器",调用它就能改仓库里的数据
+export const userUserStore = create<UserStore>((set) => ({
+     user: null,
+     setUser: (user) => set({ user }),
+     clearUser: () => set({ user: null }),
+}))
