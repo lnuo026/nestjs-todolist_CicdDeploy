71131ca4beffb9340ba0e24c2a28e4288dc605c5
Nuona Liang <lnuo026@aucklanduni.ac.nz>
2026-06-26 15:47:28 +1200

    feat: front request

---
+++ frontend/src/api/todo.ts
@@ -0,0 +1,14 @@
+import request from "./request";
+
+export const getTodos = () => request.get('/todos')
+
+export const createTodo = (data: {
+      title: string;
+      description: string;
+      priority?: string;
+     }) => request.post('/todos', data)
+
+export const updateTodo = (id: string, data: { done?: boolean; title?: string }) =>
+     request.patch(`todos/${id}`, data)
+
+export const deleteTodo = (id: string) => request.delete(`todos/${id}`)
