68eca4803d4e53bbe175cfc2844ea09c217da171
Nuona Liang <lnuo026@aucklanduni.ac.nz>
2026-06-28 22:54:43 +1200

    fix: desprition add optional mark

--- frontend/src/api/todos.ts
+++ frontend/src/api/todos.ts
@@ -4,11 +4,11 @@ export const getTodos = () => request.get('/todos')
 
 export const createTodo = (data: {
       title: string;
-      description: string;
+      description?: string;
       priority?: string;
      }) => request.post('/todos', data)
 
 export const updateTodo = (id: string, data: { done?: boolean; title?: string }) =>
-     request.patch(`todos/${id}`, data)
+     request.patch(`/todos/${id}`, data)
 
-export const deleteTodo = (id: string) => request.delete(`todos/${id}`)
+export const deleteTodo = (id: string) => request.delete(`/todos/${id}`)
